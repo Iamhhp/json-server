b6831ada049f3e3189c9550c473b4a53785bc47f
Iamhhp <Iamhhp2020@gmail.com>
2024-01-13 00:09:58 +0330

    update code serverjs

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 // server.js
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults();
+
+const PORT = process.env.PORT || 3001;
 
 server.use(middlewares);
 server.use(router);
-server.listen(3000, () => {
-  console.log('JSON Server is running');
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT}`);
 });
